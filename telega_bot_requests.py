fdf2dea9ceb87f67e5d04090cbcf0e553b27011e
dima <amid.vonavi@gmail.com>
2020-02-22 23:50:18 +0200

    bot polling thread improved, added commands

--- telega_bot_requests.py
+++ telega_bot_requests.py
@@ -18,7 +18,8 @@ class BotHandler:
         self.last_replied_update_id = None
 
         self.polling_thread = threading.Thread(target=self.polling)
-        self.polling_thread_stop = False
+        self.polling_thread_stop = threading.Event()
+        self.polling_thread_stop.clear()
         self.start_bot()
 
     def get_updates(self):
@@ -32,7 +33,7 @@ class BotHandler:
             self.updates = resp.json()['result']
             if len(self.updates) > 0:
                 self.update_id = self.updates[-1]['update_id']
-                self.parse_incoming_text()
+                self.parse_incoming_message()
         else:
             print('====== {} ERROR code = {}'.format(self.get_updates.__name__, resp.status_code))
 
@@ -59,25 +60,52 @@ class BotHandler:
                     print('====== {} ERROR unknown content_type'.format(self.last_update.__name__))
         return ret
 
-    def send_message_text(self, text, chat_id, reply_to_message_id=None):
+    def send_message_text(self, text, chat_id, reply_to_message_id=None, reply_markup=None):
         method = 'sendMessage'
         querystring = {'chat_id': chat_id, 'text': text}
-        if reply_to_message_id is not None:
+        if reply_to_message_id:
             querystring.update(reply_to_message_id=reply_to_message_id)
+        if reply_markup:
+            querystring.update(reply_markup=reply_markup)
+
         resp = requests.post(self.url + method, data=querystring)
         if resp.status_code != 200:
             print('====== {} ERROR code = {}'.format(self.send_message_text.__name__, resp.status_code))
 
-    def reply_message(self, reply_text, chat_id, message_id):
-        self.send_message_text(reply_text, chat_id, message_id)
+    def reply_message(self, reply_text, chat_id, message_id, reply_markup=None):
+        self.send_message_text(reply_text, chat_id, message_id, reply_markup)
 
-    def parse_incoming_text(self):
+    def bot_command(self, cmd):
+        repl_txt = ''
+        reply_markup = None
+        if len(cmd) >= 2 and (cmd[0] == 'temp' or cmd[0] == 'темп'):
+            repl_txt = str(openweathermap.get_temp(" ".join(cmd[1:])))
+        elif cmd[0] == '/temp':
+            repl_txt = 'some text'
+            reply_markup = '{"inline_keyboard": [[{"text": "button 1", "callback_data": "service1"}, {"text": "button 2", "callback_data": "service2"}]]}'
+
+        elif cmd[0] == '/rubik':
+            resp = medicum_registr.get_free_rubik()
+            resp.insert(0, "total = {}".format(len(resp)))
+            repl_txt = "\n".join(resp)
+
+        if len(repl_txt) > 0:
+            self.reply_message(repl_txt,
+                               self.last_update(content_type='chat_id'),
+                               self.last_update(content_type='message_id',),
+                               reply_markup=reply_markup)
+
+    def parse_incoming_message(self):
         if self.last_replied_update_id is None or self.last_replied_update_id < self.update_id:
             self.last_replied_update_id = self.update_id
             text = self.last_update(content_type='text').lower().split()
 
             repl_txt = ''
-            if len(text) >= 2 and (text[0] == 'temp' or text[0] == 'темп'):
+            list_of_cmds = ['/rubik', '/temp', '/menu']
+            if text[0] in list_of_cmds:
+                self.bot_command(text)
+
+            elif len(text) >= 2 and (text[0] == 'temp' or text[0] == 'темп'):
                 repl_txt = str(openweathermap.get_temp(" ".join(text[1:])))
 
             elif text[0] == 'rubik':
@@ -94,10 +122,10 @@ class BotHandler:
         self.polling_thread.start()
 
     def stop_bot(self):
-        self.polling_thread_stop = True
+        self.polling_thread_stop.set()
 
     def polling(self):
-        while not self.polling_thread_stop:
+        while not self.polling_thread_stop.is_set():
             sleep(self.poll_period)
             self.get_updates()
 

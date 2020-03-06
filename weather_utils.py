42ebe06329ba8998b125c1824f25e255039fc661
dima <amid.vonavi@gmail.com>
2020-03-06 23:42:58 +0200

    using api resp cache

--- weather_utils.py
+++ weather_utils.py
@@ -9,30 +9,46 @@ from telega_bot import geonames_conf
 
 regions_dict = {'Europe': 'EU', 'Americas': 'NA', 'Asia': 'AS', 'Africa': 'AF', 'Oceania': 'OC'}  # TODO: check others
 
+# Cities/Countries cache
+cached_cities_api_call = {}
+cached_countries_api_call = {}
+
 
 def dict_by_api_param(param_val, param_name):
-    if param_name == 'cities':
-        url = 'http://api.geonames.org/searchJSON?country={}&cities=cities15000&username={}'. \
-            format(param_val, geonames_conf.API_TOKEN)
-        key1_in_resp = 'name'
-        key2_in_resp = 'geonameId'
-    elif param_name == 'countries':
-        url = "http://api.geonames.org/searchJSON?continentCode={}&username={}". \
-            format(param_val, geonames_conf.API_TOKEN)
-        key1_in_resp = 'countryName'
-        key2_in_resp = 'countryCode'
-    else:
-        url = None
-        key1_in_resp = None
-        key2_in_resp = None
-
     ret_dict = {}
-    if url:
-        response = requests.request("GET", url)
 
-        for resp in response.json()['geonames'][1:]:
-            ret_dict.setdefault(resp[key1_in_resp], resp[key2_in_resp])
+    #  Check if cached values can be used
+    if param_name == 'cities' and param_val in cached_cities_api_call.keys():
+        ret_dict = cached_cities_api_call[param_val]
+    elif param_name == 'countries' and param_val in cached_countries_api_call.keys():
+        ret_dict = cached_countries_api_call[param_val]
+    else:  # No cache match
+        if param_name == 'cities':
+            url = 'http://api.geonames.org/searchJSON?country={}&cities=cities15000&username={}'. \
+                format(param_val, geonames_conf.API_TOKEN)
+            key1_in_resp = 'name'
+            key2_in_resp = 'geonameId'
+        elif param_name == 'countries':
+            url = "http://api.geonames.org/searchJSON?continentCode={}&username={}". \
+                format(param_val, geonames_conf.API_TOKEN)
+            key1_in_resp = 'countryName'
+            key2_in_resp = 'countryCode'
+        else:
+            url = None
+            key1_in_resp = None
+            key2_in_resp = None
 
+        if url:
+            response = requests.request("GET", url)
+
+            for resp in response.json()['geonames'][1:]:
+                ret_dict.setdefault(resp[key1_in_resp], resp[key2_in_resp])
+
+        #  Add to cache last API resp
+        if param_name == 'cities':
+            cached_cities_api_call.setdefault(param_val, ret_dict)
+        elif param_name == 'countries':
+            cached_countries_api_call.setdefault(param_val, ret_dict)
     return ret_dict
 
 
